831d05039d4cd9e295b665f3159fb28f76f60ae6
fredericohorst <frederico.horst@gmail.com>
2019-10-20 20:08:02 -0300

    git teste 2

---
+++ 3_time_complexity_tape_equilibrium.py
@@ -0,0 +1,47 @@
+
+# TIME COMPLEXITY - TAPE EQUILIBRIUM PROBLEM @ codility lessons
+# by Frederico Horst
+# source: https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
+
+def solution(A):
+    n = len(A)
+    i = 1
+    diff = []
+    # values = []
+    while i <= n:
+        left = sum(A[:i])
+        right = sum(A[i:n])
+        # values.append(i)
+        diff.append(abs(left-right))
+        if i > n:
+            break
+        i += 1
+    # return values[diff.index(min(diff))]
+    return min(diff)
+
+
+A = [3,1,2,4,3]
+
+print(solution(A))
+
+print(A[0])
+print(sum(A))
+# #
+# n = len(A)
+# i = 1
+# #
+# left = sum(A[:i])
+# right = sum(A[i:n])
+# diff = abs(left-right)
+# #
+# # print(A[:i])
+# # print(A[i:n])
+# #
+# print(left)
+# print(right)
+# print(diff)
+# #
+#
+#
+#
+#
