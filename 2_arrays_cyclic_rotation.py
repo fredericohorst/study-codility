f8161e89b0799149293001f6992d8c6b6b3a1258
fredericohorst <frederico.horst@gmail.com>
2019-10-23 21:40:54 -0300

    solucao correta.

--- 2_arrays_cyclic_rotation.py
+++ 2_arrays_cyclic_rotation.py
@@ -4,14 +4,18 @@
 # source: https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 
 def solution(A,K):
-    A_rotated = []
-    A_rotated = A[K:len(A)]
-    for i in range(K):
-        A_rotated.append(A[i])
+    A_rotated = [None] * len(A)
+
+    for i in range(len(A)):
+        A_rotated[(i + K) % len(A)] = A[i]
+
     return A_rotated
 
 
-A = [3, 8, 9, 7, 6]
+A = [3, 8, 9, 7, 6, 1]
 K = 3
 
 print(solution(A,K))
+print(solution([1, 2, 3, 4, 5], 2))
+print(solution([1, 2, 3, 4, 5], 9))
+# print(type(A_rotated))
