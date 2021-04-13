cdf0d5a31651643b2877477b7c664adfd44c1716
fredericohorst <fredericohorst@me.com>
2021-04-13 09:48:57 -0300

    solution submitted

--- lesson3_time_complexity/perm_missing_element.py
+++ lesson3_time_complexity/perm_missing_element.py
@@ -1,22 +1,18 @@
+
 def solution(A):
     # write your code in Python 3.6
-
-    if len(A)>0: A.sort()
-    else: b=1
-
-    for n in range(0,len(A)):
-        if n + 1 < A[n]:
-            b = n+1
-            print(n)
-        elif n + 1 == A[n]:
-            b = 1
-
-    return b
+    act_sum = 0
+    for number in A:
+        act_sum += number # same of using sum(A)
+    max_number = len(A) + 1
+    expected_number = (max_number * (max_number + 1) // 2) # // python integer division
+    print('act_sum:', act_sum)
+    print('expected_number:', expected_number)
+    return expected_number - act_sum
 
 
-print('example: ', solution([2, 3, 1, 5]))
-print('continuous: ', solution([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-print('empty: ', solution([]))
-print('single: ', solution([3]))
-print('double: ', solution([2,3]))
-print('double: ', solution([1,3]))
+print('example: ', solution([2, 3, 1, 5])) # should return 4
+print('continuous: ', solution([1, 2, 3, 4, 5, 6, 7, 8, 9])) # should return 10
+print('empty: ', solution([])) # should return 1
+print('two elements: ', solution([2,3])) # should return 1
+print('two elements: ', solution([1,3])) # should return 2 
