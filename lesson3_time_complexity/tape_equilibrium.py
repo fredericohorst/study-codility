c14f634779a211b30c7e27f37c2b9ddf524efd16
fredericohorst <fredericohorst@me.com>
2021-04-21 17:28:54 -0300

    solution submitted

--- lesson3_time_complexity/tape_equilibrium.py
+++ lesson3_time_complexity/tape_equilibrium.py
@@ -4,9 +4,17 @@
 # source: https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
 
 def solution(A):
+    n = len(A)
+    z = [] # min value
+    for i in range(1,n):
+        x = sum(A[0:i])
+        y = sum(A[i:n])
+        z.append(abs(x-y))
 
-    pass
+    return min(z), z.index(min(z))+1
 
 A = [3,1,2,4,3]
+print(solution(A))
 
+A = [3,4,1,8,2,10,7,8,4,3,1,9]
 print(solution(A))
