0ec0400d6af6fb35748033b68ddea375c03ba15e
fredericohorst <fredericohorst@me.com>
2021-04-26 22:16:07 -0300

    trying

--- lesson4_counting_elements/max_counters.py
+++ lesson4_counting_elements/max_counters.py
@@ -3,20 +3,27 @@
 # by Frederico Horst
 # source: https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
 
-def solution(N,A):
-    pass
+def solution(X, A):
+    river_positions = [False] * (X + 1)
+    for time in range(len(A)):
+        pos = A[time]
+        if not river_positions[pos]:
+            river_positions[time] = True
+            X -= 1
+            if X == 0: return time
+    return -1
+    # for r in range(X):
+    #     # print('r pos:', r+1)
+    #     res = [t for t in range(len(A)) if A[t] == r+1]
+    #     if len(res) != 0:
+    #         river[r] = A[min(res)]
+    #     elif len(res) == 0:
+    #         break 
+    #     # return -1
+    # return max(river)
 
 
-
-
-
-
-N = 5
-A = [3,4,4,6,1,4,4]
-# index = x - 1
-
-print(A[0])
-print(A[1])
-
-print(solution(N,A))
-
+# print(solution(5,[3,4,4,6,1,4,4])) # should be -1
+print(solution(5, [1, 3, 1, 4, 2, 3, 5, 4])) # should be 6
+# print(solution(3,[3,4,4,6,1,4,4])) # should be 
+# print(solution(10,[1,3,1,4,2,3,5,4])) # should be 
