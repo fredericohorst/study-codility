9e40a0f3cabcbd41c9cec222d93c679e4bf15595
fredericohorst <fredericohorst@me.com>
2021-04-26 23:24:30 -0300

    solution

--- lesson4_counting_elements/max_counters.py
+++ lesson4_counting_elements/max_counters.py
@@ -1,15 +1,26 @@
 
 
 def solution(N,A):
-    result = [0] * (N + 1)
-    for i in range(len(A)):
-        pos = A[i] # determinando a posição
-        if pos < N: 
-            result[pos] += 1
-        else: # max counter case
-            max_counter = max(result)
-            result = [max_counter] * (N + 1)
+    result = [0] * (N) # counters
+    start_line = 0
+    current_max = 0
 
-    return result[1:]
+    for i in A:
+        index = i - 1 # índices começam no zero
+        if i > N: # max counter case
+            start_line = current_max
+        elif result[index] < start_line:
+            result[index] = start_line + 1
+        else:
+            result[index] += 1
+        if i <= N and result[index] > current_max:
+            current_max = result[index]
+
+    for i in range(0, len(result)):
+        if result[i] < start_line:
+            result[i] = start_line
+
+    return result
 
 print(solution(5, [3,4,4,6,1,4,4]), 'expected: [3,2,2,4,2]')
+# print(solution(1, [1]), 'expected: [1]')
